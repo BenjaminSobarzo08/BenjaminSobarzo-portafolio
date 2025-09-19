a9c9237082a945fc939a5e75e3fb6dcf42e8f440
benjikpop202 <benjaminsobarzo2018@gmail.com>
2025-09-19 10:10:55 -0300

    cambiando url de cv

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ const Footer = ()=>{
     return(
         <footer>
                 <div className="body">
-              <a href="https://drive.google.com/file/d/1tpKlP328bprspPcXxbtNdyz6J2rTHTL6/view?usp=sharing"><span className='linkCard'><img src="icons\curriculum.png" alt="Curriculum" /><p>CV</p></span></a>
+              <a href="https://drive.google.com/file/d/1kCFiqsRk1BW74GUECsUVgcAZXxwM_KNL/view?usp=sharing"><span className='linkCard'><img src="icons\curriculum.png" alt="Curriculum" /><p>CV</p></span></a>
                <a href="https://www.linkedin.com/in/benjamin-sobarzo-5532a331a/"> <span className='linkCard'><img src="icons/logotipo-de-linkedin.png" alt="Linkedin" /><p>Linkedin</p></span></a>
                <a href="https://github.com/BenjaminSobarzo08"><span className='linkCard'><img src="icons\github.png" alt="GitHub" /><p>GitHub</p></span></a>
                 </div>
