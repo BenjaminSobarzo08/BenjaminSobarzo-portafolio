d5944328996093f718d6cd3b74ce826e758e6e0d
benjikpop202 <benjaminsobarzo2018@gmail.com>
2026-04-22 16:50:18 -0300

    cambiando enlace de CV

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ const Footer = ()=>{
     return(
         <footer>
                 <div className="body">
-              <a href="https://docs.google.com/document/d/1DbJaUsyLi5N2abX6wCbhMnnKp1DDgUhvAocLp8Ju5VE/edit?usp=sharing"><span className='linkCard'><img src="icons\curriculum.png" alt="Curriculum" /><p>CV</p></span></a>
+              <a href="https://drive.google.com/file/d/1ZBlXp5DZF6yS1YykK6wDZ4HnIfoqfib7/view?usp=sharing"><span className='linkCard'><img src="icons\curriculum.png" alt="Curriculum" /><p>CV</p></span></a>
                <a href="https://www.linkedin.com/in/benjamin-sobarzo-5532a331a/"> <span className='linkCard'><img src="icons/logotipo-de-linkedin.png" alt="Linkedin" /><p>Linkedin</p></span></a>
                <a href="https://github.com/BenjaminSobarzo08"><span className='linkCard'><img src="icons\github.png" alt="GitHub" /><p>GitHub</p></span></a>
                 </div>
